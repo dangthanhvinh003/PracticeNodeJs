4331a9f77a271f64b7a613dade7996ed77257266
dangthanhvinh003 <dvih003@gmail.com>
2025-03-14 21:46:51 +0700

    product controllercontroller

--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -9,5 +9,33 @@ const getAllProduct = async (req, res) => {
     data: result,
   });
 };
+const getProductById = async (req, res) => {
+  let id = req.params.id;
+  let result = await Product.find({ userId: id });
+  console.log(result);
+  //res.render("Homepage", { ListUser: result });
+  return res.status(200).json({
+    data: result,
+  });
+};
 
+const postUpdateProduct = async (req, res) => {
+  let id = req.body.id;
+  let name = req.body.name;
+  let img = req.body.img;
+  let type = req.body.type;
+  let price = req.body.price;
+  let quantity = req.body.quantity;
+
+  //const { id, name, email, img } = req.body;
+  let result = await Product.updateOne(
+    { productId: id },
+    { name: name, img: img, type: type, price: price, quantity: quantity }
+  );
+  console.log(result);
+
+  return res.status(200).json({
+    data: result,
+  });
+};
 module.exports = { getAllProduct };
